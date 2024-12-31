ac161e62863828afe7e9e9cfb7f079117c8536cf
dendydandees <dendy@fisiohome.id>
2024-12-31 02:00:11 +0000

    feat: implement service worker registration and enhance push notification handling

--- app/frontend/entrypoints/application.ts
+++ app/frontend/entrypoints/application.ts
@@ -29,3 +29,15 @@ console.log(
 
 // Example: Import a stylesheet in app/frontend/index.css
 // import '~/index.css'
+
+if ('serviceWorker' in navigator) {
+	// Register the service worker
+	navigator.serviceWorker
+		.register('/service-worker.js')
+		.then((registration) => {
+			console.log('Service Worker registered with scope:', registration.scope);
+		})
+		.catch((error) => {
+			console.log('Service Worker registration failed:', error);
+		});
+}
--- app/views/pwa/service-worker.js
+++ app/views/pwa/service-worker.js
@@ -1,23 +1,40 @@
 // Add a service worker for processing Web Push notifications:
-//
-// self.addEventListener("push", async (event) => {
-//   const { title, options } = await event.data.json()
-//   event.waitUntil(self.registration.showNotification(title, options))
-// })
-//
-// self.addEventListener("notificationclick", function(event) {
+
+// The install event is fired when the service worker is first installed
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+});
+
+// The activate event is fired after the install event when the service worker is actually controlling the page
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+});
+
+self.addEventListener("push", async (event) => {
+  /**
+   * Extracts the title and options from the event data.
+   *
+   * @param {Event} event - The event object containing the data.
+   * @returns {Promise<{title: string, options: Object}>} A promise that resolves to an object containing the title and options.
+   */
+  console.log(await event.data)
+  const { title, options } = await event.data.json()
+  event.waitUntil(self.registration.showNotification(title, options))
+})
+
+// self.addEventListener("notificationclick", function (event) {
 //   event.notification.close()
 //   event.waitUntil(
 //     clients.matchAll({ type: "window" }).then((clientList) => {
 //       for (let i = 0; i < clientList.length; i++) {
 //         let client = clientList[i]
 //         let clientPath = (new URL(client.url)).pathname
-//
+
 //         if (clientPath == event.notification.data.path && "focus" in client) {
 //           return client.focus()
 //         }
 //       }
-//
+
 //       if (clients.openWindow) {
 //         return clients.openWindow(event.notification.data.path)
 //       }
