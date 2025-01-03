cd269c053afa100833540a44a0ac953d167a5e25
dendydandees <dendy@fisiohome.id>
2025-01-03 06:17:46 +0000

    chore: update mailer configuration for development, production, and staging environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,14 +38,16 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Set localhost to be used by links generated in mailer templates.
-  # config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-  config.action_mailer.default_url_options = {
-    host: "smtp.resend.com",
-    port: 465,
-    user_name: "resend",
-    password: Rails.application.credentials.resend.api_key,
-    protocol: "https"
-  }
+  config.action_mailer.default_url_options = {host: "localhost", port: 3000}
+  # config.action_mailer.default_url_options = {
+  #   # host: "smtp.resend.com",
+  #   # port: 465,
+  #   host: "localhost",
+  #   port: 3000,
+  #   user_name: "resend",
+  #   password: Rails.application.credentials.resend.api_key,
+  #   protocol: "https"
+  # }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
