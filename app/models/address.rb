9b6af95aa3169bdac5f1f4fc0d48a9b03ee38d52
dendydandees <dendy@fisiohome.id>
2025-01-24 02:50:27 +0000

    test: enhance address model tests for coordinate updates and validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -6,7 +6,6 @@ class Address < ApplicationRecord
   has_many :therapists, through: :therapist_addresses
 
   # * cycle callbacks
-  # before_save :geocode, if: -> { address_changed? || latitude.blank? || longitude.blank? }
   before_save :update_coordinates, if: -> { latitude_changed? || longitude_changed? }
 
   # * define the validation
--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -50,10 +50,33 @@ class AddressTest < ActiveSupport::TestCase
   end
 
   # Test callbacks
-  test "update_coordinates should set coordinates as an array of latitude and longitude" do
+  test "update_coordinates should set coordinates as an array of latitude and longitude when latitude or longitude changes" do
+    # Case 1: Change latitude and longitude
     @jakarta_address.latitude = -6.2277
     @jakarta_address.longitude = 106.8414
     @jakarta_address.save!
-    assert_equal [-6.2277, 106.8414], @jakarta_address.coordinates
+    assert_equal [-6.2277, 106.8414], @jakarta_address.coordinates, "Coordinates should update correctly when both latitude and longitude change"
+
+    # Case 2: Change only latitude
+    @jakarta_address.latitude = -6.3000
+    @jakarta_address.save!
+    assert_equal [-6.3000, 106.8414], @jakarta_address.coordinates, "Coordinates should update correctly when only latitude changes"
+
+    # Case 3: Change only longitude
+    @jakarta_address.longitude = 107.0000
+    @jakarta_address.save!
+    assert_equal [-6.3000, 107.0000], @jakarta_address.coordinates, "Coordinates should update correctly when only longitude changes"
+
+    # Case 4: No changes to latitude or longitude
+    coordinates_before = @jakarta_address.coordinates.dup
+    @jakarta_address.save!
+    assert_equal coordinates_before, @jakarta_address.coordinates, "Coordinates should not change if latitude and longitude remain unchanged"
+
+    # Case 5: Test with nil latitude and longitude
+    @jakarta_address.latitude = nil
+    @jakarta_address.longitude = nil
+    assert_not @jakarta_address.valid?, "Address should not be valid without latitude and longitude"
+    assert_includes @jakarta_address.errors[:latitude], "can't be blank", "Validation should ensure latitude is present"
+    assert_includes @jakarta_address.errors[:longitude], "can't be blank", "Validation should ensure longitude is present"
   end
 end
