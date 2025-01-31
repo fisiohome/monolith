e7f70d6fa50e81c91eb63bd1847fca3eca93e988
dendydandees <dendy@fisiohome.id>
2025-01-31 18:42:12 +0000

    refactor: restructure UpdateBrandService for improved readability and maintainability

--- app/services/admin_portal/update_brand_service.rb
+++ app/services/admin_portal/update_brand_service.rb
@@ -1,66 +1,68 @@
-class UpdateBrandService
-  def initialize(service, params)
-    @service = service
-    @params = params
-  end
-
-  def call
-    ActiveRecord::Base.transaction do
-      update_service_details
-      update_locations
-      update_packages
+module AdminPortal
+  class UpdateBrandService
+    def initialize(service, params)
+      @service = service
+      @params = params
     end
-    true
-  rescue => e
-    @service.errors.add(:base, e.message)
-    Rails.logger.error("Brand update failed: #{e.message}")
-    false
-  end
 
-  private
-
-  def update_service_details
-    unless @service.update(service_params)
-      raise ActiveRecord::Rollback, @service.errors.full_messages.to_sentence
+    def call
+      ActiveRecord::Base.transaction do
+        update_service_details
+        update_locations
+        update_packages
+      end
+      true
+    rescue => e
+      @service.errors.add(:base, e.message)
+      Rails.logger.error("Brand update failed: #{e.message}")
+      false
     end
-  end
 
-  def update_locations
-    locations_data = @params[:locations] || []
-    updated_location_ids = locations_data.pluck(:id).compact
+    private
 
-    # Remove locations not included in the update
-    @service.location_services.where.not(location_id: updated_location_ids).destroy_all
-
-    locations_data.each do |location_data|
-      location = Location.find_by(city: location_data[:city])
-      next unless location
-
-      location_service = LocationService.find_or_initialize_by(location: location, service: @service)
-      location_service.active = location_data[:active]
-      unless location_service.save
-        raise ActiveRecord::Rollback, "Failed to update locations: #{location_service.errors.full_messages.to_sentence}"
+    def update_service_details
+      unless @service.update(service_params)
+        raise ActiveRecord::Rollback, @service.errors.full_messages.to_sentence
       end
     end
-  end
 
-  def update_packages
-    packages_data = @params[:packages] || []
-    updated_package_ids = packages_data.pluck(:id).compact
+    def update_locations
+      locations_data = @params[:locations] || []
+      updated_location_ids = locations_data.pluck(:id).compact
 
-    # Remove packages not included in the update
-    @service.packages.where.not(id: updated_package_ids).destroy_all
+      # Remove locations not included in the update
+      @service.location_services.where.not(location_id: updated_location_ids).destroy_all
 
-    packages_data.each do |package_data|
-      package = @service.packages.find_or_initialize_by(id: package_data[:id])
-      package.assign_attributes(package_data.except(:id))
-      unless package.save
-        raise ActiveRecord::Rollback, "Failed to update packages: #{package.errors.full_messages.to_sentence}"
+      locations_data.each do |location_data|
+        location = Location.find_by(city: location_data[:city])
+        next unless location
+
+        location_service = LocationService.find_or_initialize_by(location: location, service: @service)
+        location_service.active = location_data[:active]
+        unless location_service.save
+          raise ActiveRecord::Rollback, "Failed to update locations: #{location_service.errors.full_messages.to_sentence}"
+        end
       end
     end
-  end
 
-  def service_params
-    @params.except(:locations, :packages)
+    def update_packages
+      packages_data = @params[:packages] || []
+      updated_package_ids = packages_data.pluck(:id).compact
+
+      # Remove packages not included in the update
+      @service.packages.where.not(id: updated_package_ids).destroy_all
+
+      packages_data.each do |package_data|
+        package = @service.packages.find_or_initialize_by(id: package_data[:id])
+        package.assign_attributes(package_data.except(:id))
+        unless package.save
+          raise ActiveRecord::Rollback, "Failed to update packages: #{package.errors.full_messages.to_sentence}"
+        end
+      end
+    end
+
+    def service_params
+      @params.except(:locations, :packages)
+    end
   end
 end
