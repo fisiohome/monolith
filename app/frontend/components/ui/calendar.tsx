331d10dbf51d1f263d5d1cd94fa2e2d2d0db1e5d
dendydandees <dendy@fisiohome.id>
2025-05-18 16:50:54 +0000

    fix: [PE-86] date and year selection in the dropdown should remain functional and reopenable after any date is selected.

--- app/frontend/components/ui/calendar.tsx
+++ app/frontend/components/ui/calendar.tsx
@@ -24,6 +24,87 @@ import { Popover, PopoverContent, PopoverTrigger } from "./popover";
 // } from "./select";
 // import { ScrollArea } from "./scroll-area";
 
+function CustomDropdown({ value, onChange, children }: DropdownProps) {
+	const [isOpen, setIsOpen] = useState(false);
+	const options = Children.toArray(children) as React.ReactElement<
+		React.HTMLProps<HTMLOptionElement>
+	>[];
+	const isMonth = useMemo(() => options.length <= 12, [options.length]);
+	const selected = options.find((child) => child.props.value === value);
+
+	const handleChange = (value: string) => {
+		const changeEvent = {
+			target: { value },
+		} as React.ChangeEvent<HTMLSelectElement>;
+		onChange?.(changeEvent);
+	};
+
+	return (
+		<Popover modal open={isOpen} onOpenChange={setIsOpen}>
+			<PopoverTrigger asChild>
+				<Button
+					variant="outline"
+					className={cn(
+						"w-full justify-between pl-3 focus:ring-0 capitalize",
+						!selected?.props?.children && "text-muted-foreground",
+					)}
+					onClick={() => setIsOpen((prev) => !prev)}
+				>
+					{selected?.props?.children}
+					<ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent
+				align="start"
+				side="bottom"
+				className={cn("p-0", isMonth ? "w-fit" : "w-fit")}
+			>
+				<Command>
+					<CommandInput placeholder="Search..." />
+					<CommandList>
+						<CommandEmpty>
+							{isMonth ? "No months found" : "No years found"}
+						</CommandEmpty>
+						<CommandGroup>
+							{options.map((option, id: number) => (
+								<CommandItem
+									key={`${option.props.value}-${id}`}
+									value={
+										option?.props?.children?.toString() ||
+										option?.props?.value?.toString() ||
+										""
+									}
+									onSelect={(currentValue) => {
+										if (isMonth) {
+											const selectedValue = options.find(
+												(option) => option?.props?.children === currentValue,
+											)?.props?.value;
+											handleChange(String(selectedValue));
+										} else {
+											handleChange(currentValue);
+										}
+										setIsOpen(false);
+									}}
+									className="capitalize"
+								>
+									<Check
+										className={cn(
+											value === option.props.value
+												? "opacity-100"
+												: "opacity-0",
+										)}
+									/>
+									{option.props.children}
+								</CommandItem>
+							))}
+						</CommandGroup>
+					</CommandList>
+				</Command>
+			</PopoverContent>
+		</Popover>
+	);
+}
+
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
 function Calendar({
@@ -83,114 +164,7 @@ function Calendar({
 				...classNames,
 			}}
 			components={{
-				Dropdown: ({ value, onChange, children }: DropdownProps) => {
-					const [isOpen, setIsOpen] = useState(false);
-					const options = Children.toArray(children) as React.ReactElement<
-						React.HTMLProps<HTMLOptionElement>
-					>[];
-					const isMonth = useMemo(() => options.length <= 12, [options.length]);
-					const selected = options.find((child) => child.props.value === value);
-					const handleChange = (value: string) => {
-						const changeEvent = {
-							target: { value },
-						} as React.ChangeEvent<HTMLSelectElement>;
-						onChange?.(changeEvent);
-					};
-
-					return (
-						// <Select
-						// 	value={value?.toString()}
-						// 	onValueChange={(value) => {
-						// 		handleChange(value);
-						// 	}}
-						// >
-						// 	<SelectTrigger className="pr-1.5 focus:ring-0">
-						// 		<SelectValue>{selected?.props?.children}</SelectValue>
-						// 	</SelectTrigger>
-						// 	<SelectContent position="popper">
-						// 		<ScrollArea className="h-80">
-						// 			{options.map((option, id: number) => (
-						// 				<SelectItem
-						// 					key={`${option.props.value}-${id}`}
-						// 					value={option.props.value?.toString() ?? ""}
-						// 				>
-						// 					{option.props.children}
-						// 				</SelectItem>
-						// 			))}
-						// 		</ScrollArea>
-						// 	</SelectContent>
-						// </Select>
-
-						<Popover open={isOpen} onOpenChange={setIsOpen}>
-							<PopoverTrigger asChild>
-								<Button
-									variant="outline"
-									className={cn(
-										"w-full justify-between pl-3 focus:ring-0 capitalize",
-										!selected?.props?.children && "text-muted-foreground",
-									)}
-								>
-									{selected?.props?.children}
-									<ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
-								</Button>
-							</PopoverTrigger>
-							<PopoverContent
-								align="start"
-								side="bottom"
-								className={cn("p-0", isMonth ? "w-[175px]" : "w-[135px]")}
-							>
-								<Command>
-									<CommandInput placeholder="Search..." />
-									<CommandList>
-										<CommandEmpty>
-											{isMonth ? "No months found" : "No years found"}
-										</CommandEmpty>
-										<CommandGroup>
-											{options.map((option, id: number) => (
-												<CommandItem
-													key={`${option.props.value}-${id}`}
-													value={
-														option?.props?.children?.toString() ||
-														option?.props?.value?.toString() ||
-														""
-													}
-													onSelect={(currentValue) => {
-														// handle on select for month
-														if (isMonth) {
-															const selectedValue = options.find(
-																(option) =>
-																	option?.props?.children === currentValue,
-															)?.props?.value;
-
-															handleChange(String(selectedValue));
-															setIsOpen(false);
-															return;
-														}
-
-														// handle on select for year
-														handleChange(currentValue);
-														// close the popover
-														setIsOpen(false);
-													}}
-													className="capitalize"
-												>
-													<Check
-														className={cn(
-															value === option.props.value
-																? "opacity-100"
-																: "opacity-0",
-														)}
-													/>
-													{option.props.children}
-												</CommandItem>
-											))}
-										</CommandGroup>
-									</CommandList>
-								</Command>
-							</PopoverContent>
-						</Popover>
-					);
-				},
+				Dropdown: CustomDropdown,
 				IconLeft: () => <ChevronLeft className="w-4 h-4" />,
 				IconRight: () => <ChevronRight className="w-4 h-4" />,
 			}}
