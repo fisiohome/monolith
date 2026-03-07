7150044bd31e60c662c95a3c79cf4f3d668754fd
dendy-fisiohome <dendy@fisiohome.id>
2026-03-07 02:17:19 +0000

    feat(api): remove unused ensure_json_request method from therapists controller

--- app/controllers/api/v1/therapists_controller.rb
+++ app/controllers/api/v1/therapists_controller.rb
@@ -5,9 +5,6 @@ module Api
       skip_before_action :authenticate_user!, only: [:feasible]
       skip_before_action :verify_authenticity_token, if: -> { request.format.json? }
 
-      # Only respond to JSON
-      # before_action :ensure_json_request, only: [:feasible]
-
       # GET /api/v1/therapists/feasible
       def feasible
         preparation = AdminPortal::PreparationNewAppointmentService.new(params)
@@ -17,12 +14,6 @@ module Api
       end
 
       private
-
-      def ensure_json_request
-        unless request.headers["Accept"]&.include?("application/json") || request.format.json?
-          render json: {error: "API only accepts JSON requests"}, status: :not_acceptable
-        end
-      end
     end
   end
 end
